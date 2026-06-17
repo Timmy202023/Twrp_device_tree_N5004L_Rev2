bc439495d65f899ef609d102bc0ab1b8cdb07c1f
Timmy202023 <86170491+Timmy202023@users.noreply.github.com>
2026-06-16 21:02:40 -0400

    Add files via upload

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,10 +1,14 @@
-# Android device tree for NUU N5004L (N5004LA)
-
-```
 #
 # Copyright (C) 2026 The Android Open Source Project
 # Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_N5004LA.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_N5004LA-user \
+    omni_N5004LA-userdebug \
+    omni_N5004LA-eng
--- device.mk
+++ device.mk
@@ -1,10 +1,8 @@
-# Android device tree for NUU N5004L (N5004LA)
-
-```
 #
 # Copyright (C) 2026 The Android Open Source Project
 # Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+LOCAL_PATH := device/nuu/N5004LA
